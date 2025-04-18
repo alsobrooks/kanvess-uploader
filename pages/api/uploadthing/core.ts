31d1d360bf1f653b326f501f96785dc1701b88b7
alsobrooks <alsobrooks@gmail.com>
2025-04-18 01:47:21 +0000

    fix: update to createRouteHandler (new UploadThing)

--- pages/api/uploadthing/core.ts
+++ pages/api/uploadthing/core.ts
@@ -1,6 +1,6 @@
-import { createNextRouteHandler } from "uploadthing/next";
+import { createRouteHandler } from "uploadthing/next";
 import { ourFileRouter } from "./index"; // assumes your main router is in uploadthing/index.ts or uploadthing.ts
 
-export const { GET, POST } = createNextRouteHandler({
+export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
 });
