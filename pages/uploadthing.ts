5277abb0ef371b7598f1ed39e78791976107f412
alsobrooks <alsobrooks@gmail.com>
2025-04-18 01:30:53 +0000

    🧹 cleanup: fully removed uploadthingNextHandler

--- pages/uploadthing.ts
+++ pages/uploadthing.ts
@@ -1,17 +1,15 @@
+// pages/api/uploadthing.ts
+
 import { createUploadthing, type FileRouter } from "uploadthing/server";
 
 const f = createUploadthing();
 
 export const ourFileRouter = {
-  kanvessUploader: f({
-    image: { maxFileCount: 88, maxFileSize: "8MB" },
-  }).onUploadComplete(async ({ file }) => {
-    console.log("Uploaded file:", file.url);
-  }),
+  imageUploader: f({ image: { maxFileSize: "4MB" } }).onUploadComplete(
+    async ({ metadata, file }) => {
+      console.log("Upload complete for:", file.url);
+    }
+  ),
 } satisfies FileRouter;
 
 export type OurFileRouter = typeof ourFileRouter;
-
-export default uploadthingNextHandler({
-  router: ourFileRouter,
-});
