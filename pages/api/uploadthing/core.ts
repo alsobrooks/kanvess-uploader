72c80ecefa25708a76bf5179048c9f5b44921614
alsobrooks <alsobrooks@gmail.com>
2025-04-18 02:03:57 +0000

    core update

--- pages/api/uploadthing/core.ts
+++ pages/api/uploadthing/core.ts
@@ -1,5 +1,7 @@
-import { ourFileRouter } from "./uploadthing"; // not '../uploadthing'
-import { ourFileRouter } from "../uploadthing"; // ✅ note: this path is correct now
+// pages/api/uploadthing/core.ts
+
+import { createRouteHandler } from "uploadthing/next";
+import { ourFileRouter } from "./uploadthing"; // ✅ correct import path
 
 export const { GET, POST } = createRouteHandler({
   router: ourFileRouter,
